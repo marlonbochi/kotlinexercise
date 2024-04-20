aa1b6850e5db2456a65e47f84785cc0c699492c1
Marlon Bochi <marlon.bueno@gmail.com>
2024-04-20 12:30:12 -0300

    Added some adjusts about text style

--- app/src/main/java/br/com/alura/devhub/MainActivity.kt
+++ app/src/main/java/br/com/alura/devhub/MainActivity.kt
@@ -11,8 +11,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import br.com.alura.devhub.ui.theme.DevHubTheme
 
@@ -29,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ProfileInformation();
+                    ProfileInformation()
                 }
             }
         }
@@ -39,10 +41,22 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun ProfileInformation() {
-    Column {
-        Image(painterResource(R.drawable.image_profile), contentDescription = "Profile image");
-        Text("Marlon Bochi");
-        Text("marlonbochi");
-        Text("Senior software developer");
+    Column (
+        modifier =Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row {
+            Image(painterResource(R.drawable.image_profile), contentDescription = "Profile image")
+        }
+        Row {
+            Text(text = "Marlon Bochi", style = MaterialTheme.typography.displayMedium, fontWeight = FontWeight.Bold)
+        }
+        Row {
+            Text("marlonbochi", style = MaterialTheme.typography.titleLarge, fontWeight = FontWeight.Bold)
+        }
+        Row {
+            Text("Senior software developer",  style = MaterialTheme.typography.titleMedium)
+        }
+
     }
 }
